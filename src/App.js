122bd3319cd4fd5c6a2ca16a4e4243eb384c4344
Alexer-glitch <alexey.sergeev.8@gmai.com>
2022-11-20 01:34:56 +0300

    Data now gets from api and all works

--- src/App.js
+++ src/App.js
@@ -12,32 +12,48 @@ import {useState} from "react";
 import {DatePicker, DesktopDatePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 import dayjs from "dayjs";
-import {getDiaryDay} from "./api"
+import {format} from 'date-fns';
+import {getAllMarks, getDiaryDay, getUserInfo} from "./api"
 import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
-import moment from "@date-io/moment";
-import {ruRU} from "@mui/material/locale";
-import { styled } from '@mui/material/styles';
-import {Locale} from "moment/moment";
-import { getDay } from 'date-fns/esm';
-import { PlayLessonOutlined } from '@mui/icons-material';
 
+const date_pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
 
-
+Date.prototype.addDays=function(d){return new Date(this.valueOf()+864E5*d);};
 function App() {
     const theme = createTheme()
-    const [date, setDate] = useState(dayjs.date);
-    const [day, setDay] = useState(null);
-    const [markTab, setMarkTab] = useState(0);
+    const [cur_date, setDate] = useState(new Date());
+    const [user, setUser] = useState(0)
+    const [allMarks, setAllMarks] = useState(0)
+    //let  = dayjs.date
+    const [day, setDay] = useState(0);
+    const [markTab, setMarkTab] = useState(1);
     function a11yProps(index) {
         return {
             id: `simple-tab-${index}`,
             'aria-controls': `simple-tabpanel-${index}`,
         };
     }
-    if(!day){
-        getDiaryDay(setDay, "DDFED2B991D7AEE62D9A8136AD98B737")
+    if(user === 0){
+        setUser(null)
+        getUserInfo(setUser,538242699)
     }
-
+    if(day === 0 && user != null && user !== 0){
+        console.log(day)
+        setDay(null)
+        getDiaryDay(setDay, user.guid)
+    }
+    if(allMarks === 0 && user != null && user !== 0){
+        console.log(day)
+        setAllMarks(null)
+        getAllMarks(setAllMarks, user.guid)
+    }
+    function getColor(mark){
+        if (mark == 5) return "#ac00ff"
+        else if (mark == 4) return "#14b400"
+        else if (mark == 3) return "#ffb700"
+        else if (mark == 2) return "#ff0000"
+        else if (mark == 1) return "#ff0000"
+    }
     function TabPanel(props) {
         const { children, value, index, ...other } = props;
 
@@ -57,10 +73,11 @@ function App() {
             </div>
         );
     }
+    const bc = "rgba(209,104,229,0.39)"
     const Lesson = (props) => {
         //num: Number, time: String, name: String, homework: String, teacher: String, marks: Number[]
         return (
-            <a>
+            <>
             <Typography style={{background: "#9400ad",
                 borderTopLeftRadius: "20px",
                 display: "flex",
@@ -69,46 +86,43 @@ function App() {
                 justifyContent: "space-between",
                 borderTopRightRadius: "20px",
                 paddingLeft: "15px",
-                paddingRight: "15px"}}><a>{props.num}</a> <a>{props.time}</a></Typography>
+                paddingRight: "15px"}}><span>{props.num}</span> <span>{props.time}</span></Typography>
 
-        <Typography style={{background: "rgba(85,54,91,0.52)",
+        <Typography style={{background: bc,
             display: "flex",
             textAlign: "left",
             paddingLeft: "15px",
-            color: "white",
             paddingRight: "15px"}}>{props.name}</Typography>
 
-        <Typography style={{background: "rgba(85,54,91,0.52)",
+        <Typography style={{background: bc,
             display: "flex",
             paddingLeft: "15px",
             textAlign: "left",
             fontSize: "80%",
-            color: "white",
             paddingRight: "15px"}}>{props.teacher}</Typography>
 
-        <Typography style={{background: "rgba(85,54,91,0.52)",
+        <Typography style={{background: bc,
             display: "flex",
             textAlign: "left",
             paddingLeft: "15px",
             fontSize: "70%",
-            color: "white",
             paddingRight: "15px"}}>{props.homework}</Typography>
-
-        <Typography style={{background: "rgba(85,54,91,0.52)",
+                {props.marks.length > 0 ?
+        <Typography style={{background: bc,
             display: "flex",
             textAlign: "left",
             paddingLeft: "15px",
-            fontSize: "90%",
-            color: "white",
-            paddingRight: "15px"}}>{props.marks.join(", ")}</Typography>
-            </a>
+            fontSize: "120%",
+            paddingRight: "15px"}}>{props.marks.map((mark) => <span style={{color: getColor(mark.VALUE)}}>{mark.VALUE + "\n"}</span>)}</Typography>:
+                <></>}
+            </>
         )
     }
     const Marks = (props) => {
         //num: Number, time: String, name: String, homework: String, teacher: String, marks: Number[]
         return (
-            <a >
-        <Typography style={{background: "rgba(85,54,91,0.52)",
+            < >
+        <Typography component={'span'}  style={{background: "rgba(209,104,229,1)",
             display: "flex",
             textAlign: "left",
             borderTopRightRadius: 20,
@@ -117,61 +131,85 @@ function App() {
             color: "white",
             paddingRight: "15px"}}>{props.lesson}</Typography>
             <div className={"marks"} style={{display: "flex", maxWidth: 1024, flexDirection: "row", overflowX: "auto"}}>
-                {props.marks.map((mark) =>
-                    <div>
-                        <Card style={{width: 50, margin: 2}}>{mark.date} <br /> {mark.mark}</Card>
+                {props.marks.map((mark, i) =>
+                    <div key={i}>
+                        <Card component={''}  style={{width: 50, margin: 2}}>{format(new Date(mark.DATE.replace(date_pattern,'$3-$2-$1')),
+                            "dd.MM")}
+                            <br /> <span style={{color: getColor(mark.VALUE)}}>{mark.VALUE}</span></Card>
                     </div>
                 )}
             </div>
-            </a>
+            </>
         )
     }
     //getDay()
+    //
   return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Дневник Pskovedu
-            </Typography>
-            <Button color="inherit">Сергеев Алексей</Button>
-              <IconButton sx={{ p: 0 }}>
-                  <Avatar src="https://pskovedu.ml/api/images/1B120E187EA450E1E1C2877F679EE1EB" />
+      <Box sx={{ flexGrow: 1 }} style={{overflowX: "clip"}}>
+        <AppBar position="sticky" style={{height: "6vh", minHeight: 50}}>
+            <Grid>
+                <Typography variant="h6" style={{display: "inline-block", position: "absolute",
+                    top: "50%", marginLeft: 6, msTransform: "translateY(-50%)", transform: "translateY(-50%)"}} sx={{flexGrow: 1}}>
+                    Дневник Pskovedu
+                </Typography>
+              <IconButton style={{display: "inline-block", position: "relative", float: "right"}}>
+                  {user ? <Avatar src={"https://pskovedu.ml/api/images/" + user.messages_guid} />
+                  :
+                  <Skeleton variant={"circular"} animation={"wave"}>
+                      <Avatar></Avatar>
+                  </Skeleton> }
+
               </IconButton>
-          </Toolbar>
+                <Button style={{height: "100%", position: "relative", float: "right"}} color="inherit">{user ? user.name : <Skeleton width={100}
+                                                                                                                                animation={"wave"}/>}</Button>
+            </Grid>
         </AppBar>
-          <Grid container spacing={2}>
+          <Grid container columnGap={0}>
           <Box  sx={{
               display: 'flex',
               flexWrap: 'wrap',
-              alignItems: "center",
+              alignItems: "stretch",
               textAlign: "center",
               verticalAlign: "middle",
               '& > :not(style)': {
-                  m: 3,
-                  width: 512,
-                  height: "100%",
+                  marginTop: "1vh",
+                  width: "35vw",
+                  height: "90vh"
               },
           }}>
-              <Paper>
-                  <LocalizationProvider dateAdapter={AdapterMoment} style={{width: 50}}>
-                      <DesktopDatePicker
-                          label="Дата"
-                          value={date}
+              <Paper style={{overflow: "scroll"}}>
+                  <Grid container columnGap={0} rowGap={4} style={{margin: "1vh"}}>
+                      <Button onClick={() => {
+                          setDate(cur_date.addDays(-1))
+                          getDiaryDay(setDay, "DDFED2B991D7AEE62D9A8136AD98B737", format(cur_date,'dd.MM.yyyy'))
+                      }}>{"<--"}</Button>
+                        <LocalizationProvider dateAdapter={AdapterMoment} >
+                            <DesktopDatePicker
+                                label="Дата"
+                                value={cur_date}
+                                minDate={dayjs.date}
+                                inputFormat="DD.MM"
+                                onChange={(date) => {
+                                    setDate(new Date(date.toString()))
+                                    getDiaryDay(setDay, "DDFED2B991D7AEE62D9A8136AD98B737", format(cur_date,'dd.MM.yyyy'))
+                                }}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
 
-                          minDate={dayjs.date}
-                          inputFormat="DD.MM"
-                          onChange={(date) => {
-                              setDate(date);
-                          }}
-                          renderInput={(params) => <TextField {...params} />}
-                      />
-                  </LocalizationProvider>
-                  <Stack spacing={2} style={{margin: "5px"}}>
-                    {day != null && day.data.map((lesson) =>
-                    <Lesson key={lesson.LESSON_NUMBER} num={lesson.LESSON_NUMBER} teacher={lesson.TEACHER_NAME} 
-                    homework={lesson.HOMEWORK_PREVIOUS.HOMEWORK} marks={lesson.MARKS}
-                    time={lesson.LESSON_TIME_BEGIN} name={lesson.SUBJECT_NAME} />)}
+                      <Button onClick={() => {
+                          setDate(cur_date.addDays(1))
+                          getDiaryDay(setDay, "DDFED2B991D7AEE62D9A8136AD98B737", format(cur_date,'dd.MM.yyyy'))
+                      }}>{"-->"}</Button>
+                  </Grid>
+                  <Stack spacing={2} style={{marginLeft: 5, marginRight: 5}}>
+                      {day !== 0 && day != null ? day.map((lesson) =>
+                          <div key={lesson.LESSON_NUMBER}>
+                    <Lesson num={lesson.LESSON_NUMBER} teacher={lesson.TEACHER_NAME}
+                    homework={lesson.HOMEWORK_PREVIOUS ? lesson.HOMEWORK_PREVIOUS.HOMEWORK : ""} marks={lesson.MARKS}
+                    time={lesson.LESSON_TIME_BEGIN + "-" + lesson.LESSON_TIME_END} name={lesson.SUBJECT_NAME} />
+                          </div>) :
+                        <Skeleton style={{height: "80vh", marginLeft: 5}} variant={"rounded"} animation={"wave"}></Skeleton>}
                   </Stack>
               </Paper>
           </Box>
@@ -182,12 +220,13 @@ function App() {
               textAlign: "center",
               verticalAlign: "middle",
               '& > :not(style)': {
-                  m: 5,
-                  width: "100%",
-                  height: "100%",
+                  m: 2,
+                  marginTop: "1vh",
+                  width: "60vw",
+                  height: "90vh",
               },
           }}>
-              <Paper>
+              <Paper style={{overflowX: "scroll"}}>
                   <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                       <Tabs value={markTab} onChange={(event, value) => setMarkTab(value)} aria-label="basic tabs example">
                           <Tab label="Четвертные оценки" {...a11yProps(0)} />
@@ -196,32 +235,17 @@ function App() {
                       </Tabs>
                   </Box>
                   <TabPanel value={markTab} index={0}>
-                      <Stack spacing={2} style={{margin: "5px"}}>
-                      <Marks lesson={"География"} marks={[{date: "12.22", mark: 1},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5}]} />
-                      <Marks lesson={"География"} marks={[{date: "12.22", mark: 1},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5}]} />
-                      <Marks lesson={"География"} marks={[{date: "12.22", mark: 1},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5},{date: "12.22", mark: 2},
-                          {date: "12.22", mark: 3},{date: "12.22", mark: 4},{date: "12.22", mark: 5}]} />
-                      </Stack>
+                      Скоро!
                   </TabPanel>
                   <TabPanel value={markTab} index={1}>
-                      Item Twodfsg
+                      <Stack spacing={2} style={{margin: "5px"}}>
+                          {allMarks !== 0 && allMarks != null ? allMarks.map((lesson) =>
+                          <Marks lesson={lesson.SUBJECT_NAME} marks={lesson.MARKS} />):
+                          <Skeleton style={{height: "76vh"}} variant={"rounded"} animation={"wave"}/>}
+                      </Stack>
                   </TabPanel>
                   <TabPanel value={markTab} index={2}>
-                      Item Thredsfg
+                      Скоро!
                   </TabPanel>
               </Paper>
           </Box>
--- src/api.js
+++ src/api.js
@@ -1,8 +1,27 @@
 import axios from "axios"
-const base_url = "https://pskovedu.ml/api/"
+const base_url = "http://127.0.0.1:8000/"
 export async function getDiaryDay(setter, guid, day){
+    setter(null)
+    console.log("fet")
+    if (day === undefined) day = ""
     let res = await axios.post(base_url + "journals/diaryday", {
+        guid: guid,
+        date: day
+    })
+    if(res.data.success === true) setter(res.data.data)
+}
+export async function getAllMarks(setter, guid){
+    setter(null)
+    console.log("fet")
+    let res = await axios.post(base_url + "journals/allmarks", {
         guid: guid
     })
-    setter(res.data)
+    if(res.data.success === true) setter(res.data.data)
+}
+export async function getUserInfo(setter, code){
+    setter(null)
+    console.log("logib")
+    let res = await axios.get(base_url + "auth/login?code=" + code.toString())
+    console.log("logged")
+    if(res.status === 200) setter(res.data)
 }
